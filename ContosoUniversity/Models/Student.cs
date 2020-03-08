3b8b3d4a41613f4ce20052eb33fb0a25a5c0729c
Smith-Brandon <mormoncook@gmail.com>
2020-03-08 15:41:40 -0700

    Commit

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        //
+        [Display(Name = "Last Name")]
+        [StringLength(120, MinimumLength = 3)]
+        [Required]
+        public string LastName { get; set; }
+        //
+
+        [Display(Name = "First Name")]
+        [StringLength(120, MinimumLength = 3)]
+        [Required]
+        public string FirstMidName { get; set; }
+        //
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        //
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
